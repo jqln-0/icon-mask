b957e4264dbf52ea80f08f37e1f9c6b3dec46500
Jacqueline Leykam <jleykam@gmail.com>
2014-06-12 18:24:56 +1000

    Rewrite draw, prepare to use exec

--- draw.go
+++ draw.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"bytes"
+	"fmt"
 	"image"
-	"image/draw"
-	_ "image/jpeg"
-	"image/png"
-	"io"
 	"log"
-	"os"
-	"sync"
+	"path"
 
-	"github.com/nfnt/resize"
+	"os"
 )
 
 func loadImage(filename string) (image.Image, error) {
@@ -31,61 +26,52 @@ func loadImage(filename string) (image.Image, error) {
 }
 
 type maskDrawer struct {
-	baseImage    image.Image
-	scaledImages map[uint]image.Image
-	scaledMutex  sync.RWMutex
-	interp       resize.InterpolationFunction
+	baseImage string
+	theme     GTKTheme
 }
 
-func CreateMaskDrawer(base string, interp resize.InterpolationFunction) *maskDrawer {
+func CreateMaskDrawer(base string, theme GTKTheme) *maskDrawer {
 	var m maskDrawer
-	img, err := loadImage(base)
-	if err != nil {
-		log.Fatalf("Failed to load base image: %v\n", err)
-	}
-	m.baseImage = img
-	m.scaledImages = make(map[uint]image.Image)
-	m.interp = interp
+	m.baseImage = base
+	m.theme = theme
 	return &m
 }
 
-func (m *maskDrawer) getScaled(size uint) image.Image {
-	m.scaledMutex.RLock()
-	img, ok := m.scaledImages[size]
-	m.scaledMutex.RUnlock()
-	if ok {
-		return img
+func createPath(outFile string) {
+	err := os.MkdirAll(path.Dir(outFile), 0775)
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	// We haven't seen this size before; scale and cache the new image.
-	defer m.scaledMutex.Unlock()
-	m.scaledMutex.Lock()
-	img = resize.Resize(size, size, m.baseImage, m.interp)
-	m.scaledImages[size] = img
-	return img
 }
 
-func (m *maskDrawer) CreateMask(icon string, size int) (io.Reader, error) {
-	// TODO: All this loading and resizing could be done concurrently.
-	img, err := loadImage(icon)
-	if err != nil {
-		return nil, err
+func (m *maskDrawer) ComposeSized(overlay string, outFile string) {
+	// Make sure the parent directory exists.
+	createPath(outFile)
+
+	fmt.Printf("composing %v\n", outFile)
+}
+
+func (m *maskDrawer) ComposeSVG(overlay string, outFile string) {
+	// Make sure the parent directory exists.
+	createPath(outFile)
+
+	fmt.Printf("composing %v\n", outFile)
+}
+
+func (m *maskDrawer) CreateIcons(icon GTKIconProperties, outdir string) {
+	// Draw one icon for each available size.
+	for _, size := range icon.Sizes {
+		sizeStr := fmt.Sprintf("%vx%v", size, size)
+		outPath := path.Join(outdir, sizeStr, "masked", icon.Name+".png")
+		filePath := m.theme.GetIcon(icon.Name, int(size))
+		m.ComposeSized(filePath, outPath)
 	}
 
-	// Make sure the icon image is the correct size.
-	if img.Bounds().Dx() != size || img.Bounds().Dy() != size {
-		// TODO: Make sure uint conversion doesn't overflow.
-		img = resize.Resize(uint(size), uint(size), img, m.interp)
+	// Draw a scaled icon if possible.
+	if icon.Scalable {
+		outPath := path.Join(outdir, "scalable", "masked", icon.Name+".svg")
+		// TODO: Replace crazy constant with a flag.
+		filePath := m.theme.GetIcon(icon.Name, 1024)
+		m.ComposeSVG(filePath, outPath)
 	}
-	baseImg := m.getScaled(uint(size))
-
-	// Perform the composition.
-	final := image.NewRGBA(baseImg.Bounds())
-	draw.Draw(final, baseImg.Bounds(), baseImg, image.ZP, draw.Src)
-	draw.Draw(final, img.Bounds(), img, image.ZP, draw.Src)
-
-	// Create and return the output reader.
-	var buf bytes.Buffer
-	png.Encode(&buf, final)
-	return &buf, nil
 }
